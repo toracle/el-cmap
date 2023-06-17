61d497a9dc8b3c01a50443335b5fe48c1b9ee3b2
Jeongsoo Park <toracle@gmail.com>
2023-06-17 21:36:07 +0900

    re-organize module

--- el-cmap.el
+++ el-cmap.el
@@ -1,6 +1,8 @@
 ;; ConcepMap for Emacs
 
 (require 's)
+(require 'el-cmap-repr)
+
 
 (defun cmap-graph-to-dot (graph)
   "digraph { }")
@@ -10,45 +12,6 @@
   `(,id ,label))
 
 
-(defun cmap-repr-digraph (graph)
-  (let* ((digraph (plist-get graph :digraph))
-         (nodes (plist-get digraph :nodes))
-         (edges (plist-get digraph :edges)))
-    (format "digraph {\n%s\n%s\n}\n"
-            (cmap-repr-nodes nodes)
-            (cmap-repr-edges edges))))
-
-
-(defun cmap-repr-node (node)
-  (let ((result nil))
-    (add-to-list 'result (format (car node) "%s"))
-    (when (cdr node)
-      (add-to-list 'result " ")
-      (add-to-list 'result (format "[label=\"%s\"]" (car (cdr node)))))
-    (add-to-list 'result ";")
-    (setq result (reverse result))
-    (apply 'concat result)))
-
-
-(defun cmap-repr-nodes (nodes)
-  (s-join "\n" (mapcar 'cmap-repr-node nodes)))
-
-
-(defun cmap-repr-edge (edge)
-  (let ((result nil))
-    (add-to-list 'result (format "%s -> %s"
-                                 (car edge)
-                                 (car (cdr edge))))
-    (when (equal (length edge) 3)
-      (add-to-list 'result " ")
-      (add-to-list 'result (format "[label=\"%s\"]" (car (last edge)))))
-    (add-to-list 'result ";")
-    (setq result (reverse result))
-    (apply 'concat result)))
-
-
-(defun cmap-repr-edges (edges)
-  (s-join "\n" (mapcar 'cmap-repr-edge edges)))
 
 
 (provide 'el-cmap)
---
+++ tests/test-el-cmap-repr.el
@@ -0,0 +1,41 @@
+(require 'el-cmap-repr)
+
+(ert-deftest cmap-repr-node ()
+  (should (equal (cmap-repr-node '("node_a"))
+                 "node_a;"))
+  (should (equal (cmap-repr-node '("node_a" "Node A"))
+                 "node_a [label=\"Node A\"];")))
+
+
+(ert-deftest cmap-repr-nodes ()
+  (should (equal (cmap-repr-nodes '(("node_a") ("node_b")))
+                 "node_a;\nnode_b;")))
+
+
+(ert-deftest cmap-repr-edge ()
+  (should (equal (cmap-repr-edge '("node_a" "node_b"))
+                 "node_a -> node_b;"))
+  (should (equal (cmap-repr-edge '("node_a" "node_b" "edge_label"))
+                 "node_a -> node_b [label=\"edge_label\"];")))
+
+
+(ert-deftest cmap-repr-edges ()
+  (should (equal (cmap-repr-edges '(("node_a" "node_b")
+                                      ("node_b" "node_c")))
+                 "node_a -> node_b;\nnode_b -> node_c;"))
+  (should (equal (cmap-repr-edges '(("node_a" "node_b" "label1")
+                                      ("node_b" "node_c" "label2")))
+                 "node_a -> node_b [label=\"label1\"];\nnode_b -> node_c [label=\"label2\"];")))
+
+
+(ert-deftest cmap-repr-digraph ()
+  (should (equal (cmap-repr-digraph nil)
+                 "digraph {\n\n\n}\n")))
+
+
+(ert-deftest repr-node-isolated-node-graph ()
+  (should (equal (cmap-repr-digraph '(:digraph (:nodes (("node_1_id" "Node 1")
+                                                        ("node_2_id" "Node 2")))))
+                 "digraph {\nnode_1_id [label=\"Node 1\"];\nnode_2_id [label=\"Node 2\"];\n\n}\n")))
+
+(provide 'test-el-cmap-repr)
