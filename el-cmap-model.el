7f9423fcf9e16dc4e1b92dc2b2d70775216d622f
Jeongsoo Park <toracle@gmail.com>
2023-06-18 22:53:51 +0900

    identify a node with node id and an edge with edge id

--- el-cmap-model.el
+++ el-cmap-model.el
@@ -1,5 +1,21 @@
-(defun cmap-node (id &optional label)
-  `(,id . ,label))
+;; Model manipulation of ConceptMap
+
+(defun cmap-node (&optional properties id)
+  "Create a node. Can optionaly give PROPERTIES, especially for give node label. And ID also for debug use."
+  (let ((node-id nil)
+        (node-label nil))
+   (if id (setq node-id id)
+     (setq node-id (org-id-uuid)))
+   (unless (plist-get properties :label)
+     (plist-put properties :label (format "%s"(gensym "New Node "))))
+   `(,node-id . ,properties)))
+
+
+(defun cmap-edge (node-a-id node-b-id &optional properties id)
+  (let ((edge-id nil))
+    (if id (setq edge-id id)
+      (setq edge-id (org-id-uuid)))
+    `(,edge-id . (,node-a-id ,node-b-id ,properties))))
 
 
 (defun cmap-init-graph ()
@@ -8,17 +24,33 @@
 
 (defun cmap-add-node (graph node)
   (let* ((digraph (plist-get graph :digraph))
-         (nodes (plist-get digraph :nodes)))
-    (plist-put digraph :nodes (add-to-list 'nodes node))
+         (nodes (plist-get digraph :nodes))
+         (node-id (car node))
+         (node-entry (assoc node-id nodes)))
+    (if node-entry
+        (setf (cdr node-entry) (cdr node))
+      (plist-put digraph :nodes (add-to-list 'nodes node)))
     graph))
+
+
+(defun cmap-get-node (graph node-id)
+  (let* ((digraph (plist-get graph :digarph))
+         (nodes (plist-get digraph :nodes))
+         (node (assoc node-id nodes)))
+    node))
 
 
 (defun cmap-add-edge (graph edge)
   (let* ((digraph (plist-get graph :digraph))
          (nodes (plist-get digraph :nodes))
-         (edges (plist-get digraph :edges)))
-    (cmap-add-node graph (car edge))
-    (cmap-add-node graph (car (cdr edge)))
+         (edges (plist-get digraph :edges))
+         (node-a-id (car edge))
+         (node-b-id (car (cdr edge))))
+    
+    (unless (cmap-get-node graph node-a-id)
+      (cmap-add-node graph (cmap-node nil node-a-id)))
+    (unless (cmap-get-node graph node-b-id)
+      (cmap-add-node graph (cmap-node nil node-b-id)))
     (plist-put digraph :edges (add-to-list 'edges edge))
     graph))
 
--- tests/test-el-cmap-model.el
+++ tests/test-el-cmap-model.el
@@ -1,6 +1,13 @@
 (require 'el-cmap-model)
 
 
+(ert-deftest cmap-node ()
+  (should (equal (cmap-node '(:label "Node A") "node-a-id")
+                 '("node-a-id" . (:label "Node A"))))
+  (should (equal (cdr (cmap-node '(:label "Node A")))
+                 '(:label "Node A"))))
+
+
 (ert-deftest cmap-init-graph ()
   (should (equal (cmap-init-graph)
                  '(:config nil :digraph (:nodes nil :edges nil)))))
@@ -8,9 +15,9 @@
 
 (ert-deftest cmap-add-node ()
   (let ((graph (cmap-init-graph)))
-    (should (equal (cmap-add-node graph (cmap-node "node_a"))
+    (should (equal (cmap-add-node graph (cmap-node '(:label "Node A") "node_a"))
                    (list :config nil
-                         :digraph (list :nodes '(("node_a"))
+                         :digraph (list :nodes '(("node_a" . (:label "Node A")))
                                         :edges nil))))))
 
 
@@ -18,7 +25,7 @@
   (let ((graph (cmap-init-graph)))
     (should (equal (cmap-add-edge graph '("node_a" "node_b"))
                    (list :config nil
-                         :digraph (list :nodes '("node_b" "node_a")
+                         :digraph (list :nodes '(("node_b") ("node_a"))
                                         :edges '(("node_a" "node_b"))))))))
 
 
