843c18b9f7a20055142539321ba989e122cebd85
Jeongsoo Park <toracle@gmail.com>
2023-06-27 00:15:09 +0900

    extract to el-cmap-render

--- el-cmap-buffer.el
+++ el-cmap-buffer.el
@@ -1,4 +1,6 @@
 (require 's)
+(require 'el-cmap-model)
+
 
 (defun cmap-save ()
   (interactive)
@@ -92,141 +94,6 @@
   (cmap))
 
 
-(defun cmap-toolbar ()
-  (insert "[")
-  (insert-button "New Graph"
-                 'follow-link "\C-m"
-                 'action '(lambda (button)
-                            (cmap-new-graph)))
-
-  (insert "] [")
-  (insert-button "Add Node"
-                 'follow-link "\C-m"
-                 'action '(lambda (button)
-                            (cmap-add-node)))
-  (insert "] [")
-  (insert-button "Add Edge"
-                 'follow-link "\C-m"
-                 'action '(lambda (button)
-                            (cmap-add-edge)))
-  (insert "] [")
-  (insert-button "Select Focal Node"
-                 'follow-link "\C-m"
-                 'action '(lambda (button)
-                            (cmap-select-focal-node)))
-  (insert "]"))
-
-
-(defun cmap-graph ()
-  (let ((edges (copy-sequence (cmap-model-get-directed-edges
-                               *cmap-graph*
-                               *cmap-focal-node-id* t))))
-    (while edges
-      (let* ((edge (pop edges))
-             (src-node-id (cadr edge))
-             (node (cmap-model-get-node *cmap-graph* src-node-id))
-             (node-label (cmap-model-get-node-prop node :label))
-             (edge-label (cmap-model-get-edge-prop edge :label)))
-        (insert "    [")
-        (insert-button (propertize node-label 'node node)
-                       'follow-link "\C-m"
-                       'action '(lambda (button)
-                                  (setq-local *cmap-focal-node-id* (car (button-get button 'node)))
-                                  (cmap))
-                       'node node)
-        (insert "] ----")
-        (when edge-label
-          (insert " ")
-          (insert-button (propertize (format "%s" edge-label) 'edge edge))
-          (insert " "))
-        (insert "----> ")
-        (insert "[")
-        (insert-button "X"
-                       'follow-link t
-                       'action '(lambda (button)
-                                  (cmap-model-remove-edge *cmap-graph* (car (button-get button 'edge)))
-                                  (cmap))
-                       'edge edge)
-        (insert "]")
-        (newline))))
-
-  (newline)
-
-  (when *cmap-focal-node-id*
-    (let* ((node (cmap-model-get-node *cmap-graph*
-                                      *cmap-focal-node-id*))
-           (node-label (cmap-model-get-node-prop node :label)))
-
-      (insert "        ----------> [")
-      (insert-button (propertize (format "%s" node-label) 'node node))
-      (insert "]")))
-
-  (newline)
-
-  (let ((edges (copy-sequence (cmap-model-get-directed-edges
-                               *cmap-graph*
-                               *cmap-focal-node-id*))))
-    (insert "                     ")
-    (insert "|")
-    (newline)
-    (while edges
-      (let* ((edge (pop edges))
-             (tgt-node-id (caddr edge))
-             (node (cmap-model-get-node *cmap-graph* tgt-node-id))
-             (node-label (cmap-model-get-node-prop node :label))
-             (edge-label (cmap-model-get-edge-prop edge :label)))
-        (insert "                     ")
-        (insert "+---")
-        (when edge-label
-          (insert " ")
-          (insert-button (format "%s" edge-label))
-          (insert " "))
-        (insert "----> [")
-        (insert-button node-label
-                       'follow-link "\C-m"
-                       'action '(lambda (button)
-                                  (setq-local *cmap-focal-node-id* (car (button-get button 'node)))
-                                  (cmap))
-                       'node node)
-        (insert "]")
-        (insert " [")
-        (insert-button "X"
-                       'follow-link t
-                       'action '(lambda (button)
-                                  (cmap-model-remove-edge *cmap-graph* (car (button-get button 'edge)))
-                                  (cmap))
-                       'edge edge)
-        (insert "]")
-
-        (newline)))))
-
-
-(defun cmap-node-list ()
-  (let ((nodes (copy-sequence (cmap-model-get-nodes *cmap-graph*))))
-    (while nodes
-      (let* ((node (pop nodes))
-             (node-label (cmap-model-get-node-prop node :label)))
-        (insert " * ")
-        (insert-button node-label
-                       'follow-link "\C-m"
-                       'action '(lambda (button)
-                                  (setq-local *cmap-focal-node-id* (car (button-get button 'node)))
-                                  (cmap))
-                       'node node)
-        (insert " [")
-        (insert-button "X"
-                       'follow-link t
-                       'action '(lambda (button)
-                                  (let ((node-id (car (button-get button 'node))))
-                                    (cmap-model-remove-node *cmap-graph* node-id)
-                                    (when (equal node-id *cmap-focal-node-id*)
-                                      (setq-local *cmap-focal-node-id* (car (first (cmap-model-get-nodes *cmap-graph*)))))
-                                    (cmap)))
-                       'node node)
-        (insert "]")
-        (newline)))))
-
-
 (defun cmap-toggle-toolbar ()
   (interactive)
   (if *cmap-toolbar-visible*
@@ -271,3 +138,5 @@
          (filename-parts (car (-drop-last 1 path-parts))))
     (when (equal old-ext (car (last path-parts)))
       (s-join "." (list filename-parts new-ext)))))
+
+(provide 'el-cmap-buffer)
